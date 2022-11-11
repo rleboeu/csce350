45119ed8dd1e5a9c565f10cbf74a29947727a26b
rleboeu <leboeufrm@gmail.com>
2022-11-10 21:34:33 -0500

    Implemented vector quicksort

--- project-5-350/LeBoeuf_Ryan_Quicksort.cpp
+++ project-5-350/LeBoeuf_Ryan_Quicksort.cpp
@@ -1,31 +1,63 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
 #include <string>
 #include <vector>
 
-using namespace std;
+int partition(std::vector<float> &arr, int low, int high) {
+	int pivot = arr.at(high);
+	int i = (low - 1);
 
-void quicksort(vector<int> values, string inputFile, string outputFile) {
+	for (int j = low; j <= high-1; j++) {
+		if (arr.at(j) <= pivot) {
+			i++;
+			std::iter_swap(arr.begin() + i, arr.begin() + j);
+		}
+	}
 
-	cout << values.size() << endl;
-	cout << inputFile << endl;
-	cout << outputFile << endl;
+	std::iter_swap(arr.begin() + (i+1), arr.begin() + high);
+	return (i+1);
+}
 
+
+
+void quicksort(std::vector<float> &arr, int low, int high) {
+	if (low < high) {
+		int pivot = partition(arr, low, high);
+		quicksort(arr, low, pivot-1);
+		quicksort(arr, pivot+1, high);
+	}
+}
+
+void print_array(std::vector<float> arr) {
+	for (auto a = arr.begin(); a != arr.end(); a++) {
+		std::cout << *a << std::endl;
+	}
+	std::cout << std::endl;
+}
+
+// split a string delimited by space into vector 
+std::vector<std::string> split(std::string s)
+{
+    std::stringstream ss(s);
+    std::string word;
+	std::vector<std::string> tokens;
+
+    while (ss >> word) {
+        tokens.push_back(word);
+    }
+
+	return tokens;
 }
 
 int main(int argc, char** argv) {
 
-    if (argc != 3) {
-        // print usage
-        cout << "Usage: ./start input.txt output.txt" << endl;
-    } else {
-        // do the thing
-		vector<int> values;
-		values.push_back(10);
-		values.push_back(20);
-		values.push_back(30);
+	std::vector<float> buffer({ 40.23, 30, 20, 50, 10.24353 });
 
-		quicksort(values, argv[1], argv[2]);
-    }
+	quicksort(buffer, 0, buffer.size()-1);
+	
+	print_array(buffer);
 
     return 0;
 }
