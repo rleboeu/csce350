18c9f1ebe10702cde6e8f38360e58cfa74cdf49b
rleboeu <leboeufrm@gmail.com>
2022-11-10 22:04:03 -0500

    File I/O support

--- project-5-350/LeBoeuf_Ryan_Quicksort.cpp
+++ project-5-350/LeBoeuf_Ryan_Quicksort.cpp
@@ -5,6 +5,9 @@
 #include <string>
 #include <vector>
 
+#define FLOAT_PRECISION 5
+#define USAGE "USAGE: ./start input.txt output.txt"
+
 int partition(std::vector<float> &arr, int low, int high) {
 	int pivot = arr.at(high);
 	int i = (low - 1);
@@ -19,8 +22,6 @@ int partition(std::vector<float> &arr, int low, int high) {
 	std::iter_swap(arr.begin() + (i+1), arr.begin() + high);
 	return (i+1);
 }
-
-
 
 void quicksort(std::vector<float> &arr, int low, int high) {
 	if (low < high) {
@@ -51,13 +52,54 @@ std::vector<std::string> split(std::string s)
 	return tokens;
 }
 
+// assume file is already open
+void vector_to_file(std::ofstream &file, std::vector<float> arr) {
+	int before_last = arr.size()-1;
+	for (auto a = arr.begin(); a != arr.end(); a++) {
+		file << std::fixed << std::setprecision(FLOAT_PRECISION) << *a;
+
+		if (std::distance(arr.begin(), a) < before_last) {
+			file << " ";
+		}
+	}
+}
+
+std::vector<float> file_to_vector(std::ifstream &file) {
+	std::vector<std::string> tokens;
+	std::vector<float> values;
+	std::string line;
+
+	std::getline(file, line);
+	tokens = split(line);
+
+	for (std::string s : tokens) {
+		values.push_back(std::stof(s));
+	}	
+
+	return values;
+}
+
 int main(int argc, char** argv) {
 
-	std::vector<float> buffer({ 40.23, 30, 20, 50, 10.24353 });
+	if (argc != 3) {
+		fprintf(stderr, "ERROR: Invalid number of arguments!\n%s\n", USAGE);
+		return 1;
+	}
+
+	std::ofstream output(argv[2]);
+	std::ifstream input(argv[1]);
+
+	//std::vector<float> buffer({ 40.23, 30, 20, 50, 10.24353 });
+	std::vector<float> buffer = file_to_vector(input);
 
 	quicksort(buffer, 0, buffer.size()-1);
 	
 	print_array(buffer);
 
-    return 0;
+	vector_to_file(output, buffer);
+
+	input.close();
+    output.close();
+	
+	return 0;
 }
